4bdac9deb2d31356c8840bef3a02e0b44773c7b3
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-12 17:26:58 +0300

    start_app_files

--- lib/features/application.dart
+++ lib/features/application.dart
@@ -1,5 +1,7 @@
 import 'package:app/di/service.dart';
-import 'package:app/features/presentation/ui/home/bloc/user_cubit.dart';
+import 'package:app/features/presentation/ui/profile/bloc/user/user_cubit.dart';
+import 'package:app/features/presentation/ui/register/bloc/register/register_cubit.dart';
+import 'package:app/features/presentation/ui/sign_in/bloc/cubit/sign_in_cubit.dart';
 import 'package:app/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:app/features/presentation/theme/theme.dart';
@@ -7,6 +9,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:app/features/presentation/bloc/theme/theme_cubit.dart';
 
 class Application extends StatefulWidget {
+  const Application({super.key});
+
   @override
   State<Application> createState() => _ApplicationState();
 }
@@ -22,7 +26,13 @@ class _ApplicationState extends State<Application> {
           create: (context) => ThemeCubit(),
         ),
         BlocProvider(
-          create: (context) => UserCubit(service()),
+          create: (context) => UserCubit(service())..init(),
+        ),
+        BlocProvider(
+          create: (context) => RegisterCubit(service())..init(),
+        ),
+        BlocProvider(
+          create: (context) => SignInCubit(service())..init(),
         ),
       ],
       child: MaterialApp.router(
---
+++ lib/features/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:app/features/application.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Center(
+        child: Lottie.asset('assets/animation/splash.json'),
+      ),
+      nextScreen: const Application(),
+    );
+  }
+}
